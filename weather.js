5a78c7e27ab1d2d8b4ba30a036ea1bd10bb194db
andrejarboe <andrejarboe@gmail.com>
2017-08-02 16:58:57 -0400

    update

--- weather.js
+++ weather.js
@@ -1,11 +1,10 @@
-
-
-$(document).ready(function(){
+$(document).ready(function() {
     // Display City Name
     $.getJSON("http://ip-api.com/json/", function(location) {
         let city = location.city;
 
         $('.location').html(city);
+        console.log(city);
     });
 
 
@@ -18,8 +17,8 @@ $(document).ready(function(){
         ];
 
         var weekdayNames = [
-            "Mon", "Tue", "Wed", 
-            "Thur", "Fri", "Sat", 
+            "Mon", "Tue", "Wed",
+            "Thur", "Fri", "Sat",
             "Sun"
         ];
 
@@ -33,14 +32,14 @@ $(document).ready(function(){
 
     date(new Date());
 
-   $('.switch').click(function (event) {
+    $('.switch').click(function(event) {
         $('.unitf').toggleClass('off on');
         $('.unitc').toggleClass('off on');
-        console.log('click');       
-       
+        console.log('click');
+
     });
 
-     unit = '&#8457';
+    unit = '&#8457';
     // Get location and add it to api
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -50,16 +49,16 @@ $(document).ready(function(){
             let lon = position.coords.longitude
 
             // Get weather
-            $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/"+apiKey+"/"+lat+","+lon, 
+            $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + lon,
                 function(weather) {
                     currentSum = weather.currently.summary;
                     icon = weather.currently.icon;
                     temp = weather.currently.temperature;
-                    
 
-                    $('.temp').html(temp+ ' ' +unit);
 
-                    switch(icon) {
+                    $('.temp').html(temp + ' ' + unit);
+
+                    switch (icon) {
                         case 'clear-day':
                             $('.weather-icon').html('<i class="wi wi-day-sunny"></i>');
                             break;
@@ -68,45 +67,45 @@ $(document).ready(function(){
                             break;
                         case 'rain':
                             $('.weather-icon').html('<i class="wi wi-rain"></i>');
-                            break;                            
+                            break;
                         case 'snow':
                             $('.weather-icon').html('<i class="wi wi-snowflake-cold"></i>');
-                            break;                            
+                            break;
                         case 'sleet':
                             $('.weather-icon').html('<i class="wi wi-sleet"></i>');
-                            break;                            
+                            break;
                         case 'wind':
                             $('.weather-icon').html('<i class="wi wi-windy"></i>');
-                            break;                            
+                            break;
                         case 'fog':
                             $('.weather-icon').html('<i class="wi wi-fog"></i>');
-                            break;                            
+                            break;
                         case 'cloudy':
                             $('.weather-icon').html('<i class="wi wi-cloudy"></i>');
-                            break;                            
+                            break;
                         case 'partly-cloudy-day':
                             $('.weather-icon').html('<i class="wi wi-day-cloudy"></i>');
-                            break;                            
+                            break;
                         case 'partly-cloudy-night':
                             $('.weather-icon').html('<i class="wi wi-night-alt-cloudy"></i>');
-                            break;                            
+                            break;
                         case 'hail':
                             $('.weather-icon').html('<i class="wi wi-hail"></i>');
-                            break;                            
+                            break;
                         case 'thunderstorm':
                             $('.weather-icon').html('<i class="wi wi-thunderstorm"></i>');
-                            break;                            
+                            break;
                         case 'tornado':
                             $('.weather-icon').html('<i class="wi wi-tornado"></i>');
                     }
                     console.log(currentSum);
                     console.log(icon);
-                });    
+                });
         });
-    } 
+    }
 
 
-  
-    
+
+
 
 });
