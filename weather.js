b40a32f0335be3e4acaf3f4bc453e5104c1deb37
andrejarboe <andrejarboe@gmail.com>
2017-08-03 12:04:01 -0400

    'update'

--- weather.js
+++ weather.js
@@ -1,111 +1,67 @@
-$(document).ready(function() {
-    // Display City Name
-    $.getJSON("https://crossorigin.me/https://ip-api.com/json/", function(location) {
-        let city = location.city;
+let unit = 'F';
+let lat;
+let lon;
 
-        $('.location').html(city);
-        console.log(city);
-    });
+$('.switch').click(function(event) {
+    $('.unitf').toggleClass('off on');
+    $('.unitc').toggleClass('off on');
 
+    if(unit === 'F'){
+        unit = 'C';
+    }else unit = 'F';
 
-    function date(date) {
-        var monthNames = [
-            "January", "February", "March",
-            "April", "May", "June", "July",
-            "August", "September", "October",
-            "November", "December"
-        ];
+    console.log('click');
+    console.log('Unit is - ' +unit);
+});
 
-        var weekdayNames = [
-            "Mon", "Tue", "Wed",
-            "Thur", "Fri", "Sat",
-            "Sun"
-        ];
+function date(date) {
+    var monthNames = [
+        "Jan.", "Feb.", "Mar.",
+        "Apr.", "May", "Jun.", "Jul.",
+        "Aug.", "Sept.", "Oct.",
+        "Nov.", "Dec."
+    ];
 
-        var weekdayIndex = date.getDay();
-        var day = date.getDate();
-        var monthIndex = date.getMonth();
-        var year = date.getFullYear();
+    var weekdayNames = [
+        "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat",
+    ];
 
-        $('#date').text(weekdayNames[weekdayIndex] + ', ' + monthNames[monthIndex] + ' ' + day + ', ' + year);
-    }
+    var weekdayIndex = date.getDay();
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
 
-    date(new Date());
+    $('#date').text(weekdayNames[weekdayIndex] + ', ' + monthNames[monthIndex] + ' ' + day + ', ' + year);
+}
 
-    $('.switch').click(function(event) {
-        $('.unitf').toggleClass('off on');
-        $('.unitc').toggleClass('off on');
-        console.log('click');
+date(new Date());
 
-    });
-
-    unit = '&#8457';
-    // Get location and add it to api
+$(document).ready(function() {    
+    // Get location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-            const apiKey = '9d0ccd8c6bf2ca3137de395f722b7d1a';
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
 
+            // Make fcc api url
+            let url = 'https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+lon;
+            // Use fcc api
+            $.getJSON(url, function(weather) {
             // Get weather
-            $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + lon,
-                function(weather) {
-                    currentSum = weather.currently.summary;
-                    icon = weather.currently.icon;
-                    temp = weather.currently.temperature;
-
-
-                    $('.temp').html(temp + ' ' + unit);
-
-                    switch (icon) {
-                        case 'clear-day':
-                            $('.weather-icon').html('<i class="wi wi-day-sunny"></i>');
-                            break;
-                        case 'clear-night':
-                            $('.weather-icon').html('<i class="wi wi-night-clear"></i>');
-                            break;
-                        case 'rain':
-                            $('.weather-icon').html('<i class="wi wi-rain"></i>');
-                            break;
-                        case 'snow':
-                            $('.weather-icon').html('<i class="wi wi-snowflake-cold"></i>');
-                            break;
-                        case 'sleet':
-                            $('.weather-icon').html('<i class="wi wi-sleet"></i>');
-                            break;
-                        case 'wind':
-                            $('.weather-icon').html('<i class="wi wi-windy"></i>');
-                            break;
-                        case 'fog':
-                            $('.weather-icon').html('<i class="wi wi-fog"></i>');
-                            break;
-                        case 'cloudy':
-                            $('.weather-icon').html('<i class="wi wi-cloudy"></i>');
-                            break;
-                        case 'partly-cloudy-day':
-                            $('.weather-icon').html('<i class="wi wi-day-cloudy"></i>');
-                            break;
-                        case 'partly-cloudy-night':
-                            $('.weather-icon').html('<i class="wi wi-night-alt-cloudy"></i>');
-                            break;
-                        case 'hail':
-                            $('.weather-icon').html('<i class="wi wi-hail"></i>');
-                            break;
-                        case 'thunderstorm':
-                            $('.weather-icon').html('<i class="wi wi-thunderstorm"></i>');
-                            break;
-                        case 'tornado':
-                            $('.weather-icon').html('<i class="wi wi-tornado"></i>');
-                    }
-                    console.log(currentSum);
-                    console.log(icon);
-                });
+            let temp = weather.main.temp;
+            let city = weather.name;
+            console.log('Unit is - ' +unit);
+            // Display location on dom
+            $('.city').html(city);
+            
+            // Display weather icon to dom 
+            // Display decsription to DOM
+            // Diplay temp to dom 
+            $('.temp').html(temp);
+            });
+            
         });
     }
 
-
-
-
-
 });
+
